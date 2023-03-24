e342a511f39d2786db8b872438982366ab354966
Prashant Singh <psingh5025615@gmail.com>
2023-03-25 00:08:02 +0530

    initial commit

--- src/components/JobDiv/Job.jsx
+++ src/components/JobDiv/Job.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { BiTimeFive } from 'react-icons/bi'
+import React from "react";
+import { BiTimeFive } from "react-icons/bi";
 
 const Job = () => {
   return (
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
         <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
-
-          <span className='flex justify-between items-center gap-4'>
-            <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white">Web Developer</h1>
-            <span>
-              <BiTimeFive/>
+          <span className="flex justify-between items-center gap-4">
+            <h1 className="text-[16px] font-semibold text-textColor group-hover:text-white">
+              Web Developer
+            </h1>
+            <span className="flex items-center text-[#ccc] gap-1">
+              <BiTimeFive />
+              Now
             </span>
-
           </span>
+          <h6 className="text-[#ccc]">Canada</h6>
+          <p className="text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio eligendi voluptas, perferendis magni vitae impedit.</p>
 
+          <div className="company flex items-center gap-2">
+            <img src="" alt="" />
+          </div>
         </div>
-        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Job
+export default Job;
