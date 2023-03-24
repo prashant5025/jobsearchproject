7383b94a57794b8c1900f3e3fd9f323bc014add3
Prashant Singh <psingh5025615@gmail.com>
2023-03-24 23:50:11 +0530

    initial commit

--- src/components/JobDiv/Job.jsx
+++ src/components/JobDiv/Job.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Job = () => {
   return (
     <div>
-      <div className="jobContainer">
+      <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
+        <div className="group">
+          
+        </div>
         
       </div>
     </div>
