d729e7996c0294f2797e3f896a15d6b7385def7a
Prashant Singh <psingh5025615@gmail.com>
2023-03-24 23:53:39 +0530

    initial commit

--- src/components/JobDiv/Job.jsx
+++ src/components/JobDiv/Job.jsx
@@ -4,7 +4,7 @@ const Job = () => {
   return (
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
-        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor">
+        <div className="group group/item singleJob w-[250px] p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
 
         </div>
         
