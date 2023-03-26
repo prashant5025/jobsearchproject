e6d2e58be28b73678fdb664eb12aff4baab53c29
Prashant Singh <psingh5025615@gmail.com>
2023-03-26 18:47:19 +0530

    initial commit

--- src/components/ValueDiv/Value.jsx
+++ src/components/ValueDiv/Value.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo1 from '../../assets/instagram.png'
+import simple from '../../assets/instagram.png'
 
 const Value = () => {
   return (
@@ -8,10 +8,30 @@ const Value = () => {
       <div className="grid gap-[10rem] grid-cols-3 items-center">
         <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
           <div className='flex items-center gap-3'>
-            <div className='imgDiv p-[40px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
-              <img src={logo1} alt='' className='w-[70%]'/>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt='' className='w-[70%]'/>
             </div>
+            <span className='font-semibold text-textColor text-[18px]'>Simplicity</span>
           </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>We believe in making things better for everyone, even if just by a little bit!</p>
+        </div>
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt='' className='w-[70%]'/>
+            </div>
+            <span className='font-semibold text-textColor text-[18px]'>Simplicity</span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>We believe in making things better for everyone,even if just by a little bit!</p>
+        </div>
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-inherit-[#dedef8] h-[40px] w-[40px] flex items-center justify-center'>
+              <img src={simple} alt='' className='w-[70%]'/>
+            </div>
+            <span className='font-semibold text-textColor text-[18px]'>Simplicity</span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>We work on the basis of creating trust which can be nurtured through authenticity</p>
         </div>
       </div>
     </div>
