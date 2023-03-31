d139e1cc6fac2d90d4b9997851300d46c9f8bf5e
Prashant Singh <psingh5025615@gmail.com>
2023-03-31 20:04:53 +0530

    initial commit

--- src/components/ValueDiv/Value.jsx
+++ src/components/ValueDiv/Value.jsx
@@ -34,6 +34,15 @@ const Value = () => {
           <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>We work on the basis of creating trust which can be nurtured through authenticity</p>
         </div>
       </div>
+
+      <div className="card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded-[10px]">
+        <div className="">
+          <h1 className='text-blueColor text-[30px] font-bold'>Ready to switch a career</h1>
+          <h2 className='text-textColor text-[25px] font-bold'>Let's get started!</h2>
+        </div>
+          <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-blueColor hover:bg-white border-blueColor'>Get Started</button>
+
+      </div>
     </div>
   )
 }
